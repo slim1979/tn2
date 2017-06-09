45f1c22b2e38e54183f0f5ffde66c905377c4df6
slim1979 <chinezan@yandex.ru>
2017-06-09 05:23:29 +0300

    Ы returned to family

--- lesson2/1.rb
+++ lesson2/1.rb
@@ -1,16 +1,4 @@
-month = {
-  jan: 31,
-  feb: 28,
-  mar: 31,
-  apr: 30,
-  may: 31,
-  jun: 30,
-  jul: 31,
-  aug: 31,
-  sept: 30,
-  oct: 31,
-  nov: 30,
-  dec: 31
-}
+month = { jan: 31, feb: 28, mar: 31, apr: 30, may: 31, jun: 30,
+          jul: 31, aug: 31, sept: 30, oct: 31, nov: 30, dec: 31 }
 
 month.each { |key, value| puts "#{key} = #{value}" if value == 30 }
