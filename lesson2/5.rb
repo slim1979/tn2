f82a7545e4e7f73815a1b9a260e75cd3864af08f
slim1979 <chinezan@yandex.ru>
2017-06-09 21:56:25 +0300

    All bugs fixed

--- lesson2/5.rb
+++ lesson2/5.rb
@@ -1,4 +1,4 @@
-arr = %i[ 31 28 31 30 31 30 31 31 30 31 30 31 ]
+arr = %w[31 28 31 30 31 30 31 31 30 31 30 31]
 
 puts 'Введите дату в формате ДД.ММ.ГГГГ:'
 print 'день: '
@@ -11,15 +11,18 @@ year = gets.chomp.to_i
 # проверка введенных данных на валидность
 if !day.zero? && day > 0 && day < 32 && !month.zero? && month > 0 && month < 13 && !year.zero? && year > 0
 
-# поправка на високосный год
-arr[1] = 29 if (year % 4).zero? || (year % 400).zero?
-count = 0
+  # поправка на високосный год
+  arr[1] = 29 if (year % 4).zero? && !(year % 100).zero? || (year % 400).zero?
 
-# магия Ruby
-arr.each do |key, value|
-  count += value if key < month
-end
-puts "Этот день в году #{count + day} по счету"
+  count = 0
+  i = 1
+
+  while i < month
+    count += arr[i-1].to_i
+    i += 1
+  end
+
+  puts "Этот день в году #{count + day} по счету"
 
 # обработка неправильного ввода
 else
