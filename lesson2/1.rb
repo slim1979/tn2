89d65dfaec5a4bb29f2434a0b6eeb3ef1105bee0
slim1979 <chinezan@yandex.ru>
2017-06-09 22:11:42 +0300

    Bug fixed

--- lesson2/1.rb
+++ lesson2/1.rb
@@ -1,4 +1,4 @@
 year = { jan: 31, feb: 28, mar: 31, apr: 30, may: 31, jun: 30,
           jul: 31, aug: 31, sept: 30, oct: 31, nov: 30, dec: 31 }
 
-year.each { |month, days| puts "#{month} = #{days}" if value == 30 }
+year.each { |month, days| puts "#{month} = #{days}" if days == 30 }
