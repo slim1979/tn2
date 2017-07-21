0c1e95e94c3239eeb9d4695c0975dcd2a23f1549
slim1979 <chinezan@yandex.ru>
2017-07-21 19:26:19 +0300

    module Accessors refactored

---
+++ lesson9/modules/acessors.rb
@@ -0,0 +1,25 @@
+# module Accessors
+module Accessors
+  def attr_accessor_with_history(*args)
+    args.each do |arg|
+      arr = "#{arg}_arr".to_sym
+      arr = []
+      var_name = "@#{arg}".to_sym
+      define_method(arg) { instance_variable_get(var_name) }
+      define_method("#{arg}=") do |value|
+        instance_variable_set(var_name, value)
+        arr << value
+      end
+      define_method("#{arg}_history") { arr }
+    end
+  end
+
+  def strong_attr_accessor(attr_name, attr_class)
+    var_name = "@#{attr_name}".to_sym
+    define_method(attr_name) { instance_variable_get(var_name) }
+    define_method("#{attr_name}=") do |value|
+      instance_variable_set(var_name, value) if value.is_a? attr_class
+      raise ArgumentError, "Принимаются значения только класса #{attr_class}" unless value.is_a? attr_class
+    end
+  end
+end
